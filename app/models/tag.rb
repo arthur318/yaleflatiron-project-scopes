ca59167a9352a339fa0626c9ef59bfd6dc07d7b4
arthur318 <arthurhwang318@gmail.com>
2020-06-30 16:57:11 -0700

    added tag stuff and sessions

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,8 @@
 class Tag < ApplicationRecord
     has_many :job_tags
     has_many :jobs, through: :job_tags
+
+    validates :name, presence: true, uniqueness: true
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'tags/new'
+  get 'tags/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :jobs, only: [:index, :show]
-  resources :tags, only: [:index, :show]
+  resources :tags, only: [:index, :show, :new, :create, :edit, :update]
 end
