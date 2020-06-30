c1ab40bbe5b6c88706b3bf7d3b8ae57be294ff79
arthur318 <arthurhwang318@gmail.com>
2020-06-30 12:44:12 -0700

    initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u1 = User.create(name: "Arthur", username: "Arthur", email: "arthur318@yahoo.com", password_digest: "password1")
+u2 = User.create(name: "Jack", username: "Jack", email: "mapeiyao@gmail.com")
+
+  # t.string "name"
+  # t.string "username"
+  # t.string "password_digest"
+  # t.string "email"
+
+company1 = Company.create(name: "Google", website: "https://www.google.com/")
+company2 = Company.create(name: "Facebook", website: "https://www.facebook.com/")
+
+job1 = Job.create(user_id: u1.id, company_id: company1.id, role: "Engineer", status: "Prospect", location: "San Francisco")
+job2 = Job.create(user_id: u2.id, company_id: company1.id, role: "Engineer", status: "Prospect", location: "San Francisco")
+job3 = Job.create(user_id: u2.id, company_id: company2.id, role: "Analyst", status: "Prospect", location: "Menlo Park")
+
+# t.bigint "user_id"
+# t.bigint "company_id"
+# t.string "role"
+# t.string "status"
+# t.string "location"
+# t.string "url"
+# t.datetime "deadline"
