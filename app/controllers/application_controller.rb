33e9bfad4818d14e015f816eb5c77a4031e982af
CelestialEmperor <mapeiyao@gmail.com>
2020-07-02 17:02:05 +0100

    Minor changes but CSS stylesheet still broken.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
 
     before_action :authenticated
 
+    layout "application"
+
     def current_user
         if session[:user_id]
             current_user = User.find(session[:user_id])
