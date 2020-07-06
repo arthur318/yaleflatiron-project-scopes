c13ac26505a8971ee08a4865bd01a87a0ae24572
CelestialEmperor <mapeiyao@gmail.com>
2020-07-06 22:09:03 +0100

    Updated CRUD for contacts & companies.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,12 @@
 class ContactsController < ApplicationController
+
     def new
         @contact = Contact.new
     end
 
     def create
         Contact.create(contact_params)
-        redirect_to "/jobs"
+        redirect_to "/companies"
     end
 
     def show
@@ -24,7 +25,16 @@ class ContactsController < ApplicationController
         @contact = Contact.find(params[:id])
     end
 
-
+    def edit
+        @user = User.find(session[:user_id])
+        @contact = Contact.find(params[:id])
+    end
+  
+    def update
+        @contact = Contact.find(params[:id])
+        @contact.update(contact_params)
+        redirect_to contact_path(@contact)
+    end
 
     private
 
@@ -33,8 +43,6 @@ class ContactsController < ApplicationController
     end
 
     def contact_params
-        # defaults = {company_id: current_company.id}
-        # .reverse_merge(defaults)
         params.require(:contact).permit(:company_name, :company_id, :first_name, :last_name, :title, :email, :phone, :notes)
     end
 
